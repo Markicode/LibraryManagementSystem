9fb129a2ca9b79502ceeac2992a70d6e65c04a53
Markicode <markschwering@hotmail.com>
2025-02-06 16:29:07 +0100

    Server menu update.

--- ServerApplication/Server.cs
+++ ServerApplication/Server.cs
@@ -21,11 +21,12 @@ public class Server
         settingsMenuOptionsAssignment = new Dictionary<settingsMenuOptions, string>()
         {
             {settingsMenuOptions.SetIp, "Change the server IP-address."},
-            {settingsMenuOptions.SetPort, "Change the server port."}
+            {settingsMenuOptions.SetPort, "Change the server port."},
+            {settingsMenuOptions.Exit, "Exit to main menu."}
         };
 
         mainMenuList = new List<mainMenuOptions> {mainMenuOptions.Settings, mainMenuOptions.StartListening, mainMenuOptions.ViewClients};
-        settingsMenuList = new List<settingsMenuOptions> {settingsMenuOptions.SetIp, settingsMenuOptions.SetPort};
+        settingsMenuList = new List<settingsMenuOptions> {settingsMenuOptions.SetIp, settingsMenuOptions.SetPort, settingsMenuOptions.Exit};
     } 
     public enum mainMenuOptions
     {   
@@ -34,27 +35,84 @@ public class Server
 
     public enum settingsMenuOptions
     {
-        SetIp = 0, SetPort = 1
+        SetIp = 0, SetPort = 1, Exit = 3
     }
 
     public void ShowMainMenu()
     {
-        Console.WriteLine("Here are your menu options:");
-        for(int i = 0; i<mainMenuList.Count; i++)
+        Dictionary<int, mainMenuOptions> mainMenu = this.MakeDictionary(mainMenuList);
+        Console.WriteLine("Main menu options:");
+        foreach(KeyValuePair<int, mainMenuOptions> mainOption in mainMenu)
         {
-            Console.WriteLine((i+1).ToString() + ". " + mainMenuOptionsAssignment[mainMenuList[i]]);
+            string choiseText = string.Format("{0}. {1, -5}", mainOption.Key, mainOption.Value.ToString());
+            Console.WriteLine(choiseText);
         }
-        validateInput(Console.ReadLine(), "main");
+        int mainChoise = ValidateMenuChoise(mainMenuList.Count);
+        mainMenuOptions chosenOption = mainMenu[mainChoise];
+        switch (chosenOption)
+        {
+            case mainMenuOptions.StartListening:
+                {
+                    this.isListening = true;
+                    Console.WriteLine("Started listening. \r\n");
+                    this.updateMainMenu();
+                    this.ShowMainMenu();
+                    break;
+                }
+            case mainMenuOptions.StopListening:
+                {
+                    this.isListening = false;
+                    Console.WriteLine("Stopped listening. \r\n");
+                    this.updateMainMenu();
+                    this.ShowMainMenu();
+                    break;
+                }
+            case mainMenuOptions.Settings:
+                {
+                    this.ShowSettingsMenu();
+                    break;
+                }
+            case mainMenuOptions.ViewClients:
+                {
+                    this.ViewClients();
+                    break;
+                }
+        }
+
     }
 
     public void ShowSettingsMenu()
     {
-        Console.WriteLine("Here are your menu options:");
-        for(int i = 0; i<settingsMenuList.Count; i++)
+        Dictionary<int, settingsMenuOptions> settingsMenu = this.MakeDictionary(settingsMenuList);
+        Console.WriteLine("Settings menu options:");
+        foreach (KeyValuePair<int, settingsMenuOptions> settingOption in settingsMenu)
         {
-            Console.WriteLine((i+1).ToString() + ". " + settingsMenuOptionsAssignment[settingsMenuList[i]]);
+            string choiseText = string.Format("{0}. {1, -5}", settingOption.Key, settingOption.Value.ToString());
+            Console.WriteLine(choiseText);
         }
-        validateInput(Console.ReadLine(), "settings");
+        int settingChoise = ValidateMenuChoise(settingsMenuList.Count);
+        settingsMenuOptions chosenOption = settingsMenu[settingChoise];
+        switch (chosenOption)
+        {
+            case settingsMenuOptions.SetIp:
+                {
+                    Console.WriteLine("Set IP here \r\n");
+                    this.ShowSettingsMenu();
+                    break;
+                }
+            case settingsMenuOptions.SetPort:
+                {
+                    Console.WriteLine("Set Port here \r\n");
+                    this.ShowSettingsMenu();
+                    break;
+                }
+            case settingsMenuOptions.Exit:
+                {
+                    Console.WriteLine("Exiting to main menu. \r\n");
+                    this.ShowMainMenu();
+                    break;
+                }
+        }
     }
 
     public void updateMainMenu()
@@ -151,26 +209,49 @@ public class Server
 
     }
 
-    /*public Task ListenForInput()
+    private Dictionary<int, TValue> MakeDictionary<TValue>(List<TValue> list)
     {
-        Task listenForInputTask = Task.Run(() => 
+        Dictionary<int, TValue> dictionary = new Dictionary<int, TValue>();
+        for (int i = 1; i <= list.Count; i++)
         {
-            while(true)
-            {
-                if(Console.ReadLine() != null)
-                {
-                    Console.ReadLine();
-                }
-            }
-        });
-
-        return listenForInputTask;
+            // object 0 uit de list, wordt optie 1 in het menu.
+            dictionary.Add(i, list[i - 1]);
+        }
+        return dictionary;
     }
 
-    public void ReturnOutput(string input)
+    private int ValidateMenuChoise(int NumberOfChoises)
     {
+        bool validChoise = false;
+        int choise = 0;
+        do
+        {
+            try
+            {
+                choise = Int32.Parse(Console.ReadLine());
+                if (choise > 0 && choise <= NumberOfChoises)
+                {
+                    validChoise = true;
+                    return choise;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a number from the menu:");
+                }
+            }
+            catch (FormatException formEx)
+            {
+                Console.WriteLine("Unvalid choise, enter a number:");
+            }
+            catch (OverflowException overflowEx)
+            {
+                Console.WriteLine("Number is too big, enter a valid number from the menu:");
+            }
+        }
+        while (!validChoise);
 
-    }*/
+        return choise;
+    }
 
 
 }
